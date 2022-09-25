23eb369750606b8810ab1a0aaab368fa155243d4
mohit040 <mohit.chourasia2020@vitstudent.ac.in>
2022-09-25 21:58:08 +0530

    adjusting for heroku

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
       <Router>
         
         {
-          domainArr.length===1 ? 
+          domainArr.length===2 ? 
           <Routes>
           <Route path={"/executive/*"} element={<AdminE/>} ></Route> 
           <Route exact path="/*" element={<PublicMain/>} >   </Route>
